d967e566bb6daeb1f9e300646b6f8276620bb003
Himani Manglik <himanimanglik@Himanis-Air.home>
2020-08-25 23:28:04 -0400

    did some of gauge function

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -10,12 +10,14 @@ function init() {
         // console.log(initId);
         Plot(initId);
         demographics(initId);
+        // gauge(initId);
 })
 }
 
 function optionChanged(id) {
     Plot(id);
     demographics(id);
+    // gauge(id);
 }
 
 
@@ -23,7 +25,7 @@ function Plot(id) {
     d3.json("samples.json").then(function(data) {
         // console.log(data);
         var samples = data.samples
-        var samples = samples.filter(sampleID => sampleID.id ===id)[0];
+        var samples = samples.filter(sampleID => sampleID.id === id)[0];
         // console.log(samples);
         var sampleValues = samples.sample_values.slice(0, 10).reverse();
         var otuIds = samples.otu_ids.slice(0, 10).reverse();
@@ -44,7 +46,6 @@ function Plot(id) {
             title: "Top 10 OTUs",
             xaxis: {title: "Sample Values"},
             yaxis: {title: "OTU"}
-            
         }
 
         Plotly.newPlot("bar", data, layout);
@@ -62,9 +63,7 @@ function Plot(id) {
         
         var layout = {
             xaxis:{title: "OTU IDs"},
-            yaxis:{title: "Sample Values"},
-            height: 600,
-            width: 1300
+            yaxis:{title: "Sample Values"}
         };
         
         var data = [trace1];
@@ -73,6 +72,54 @@ function Plot(id) {
     });
 }
 
+// function gauge (id) {
+//     d3.json("samples.json").then(function(data) {
+//         // console.log(data);
+//         var samples = data.metadata
+//         var samples = samples.filter(sampleID => sampleID.id == id)[0];
+//         console.log(samples)
+//         level = parseFloat(samples.wfreq) * 20 
+//         var degrees = 180-(level);
+//             radius = .5;
+//         var radians = degrees * Math.PI / 180;
+//         var x = radius * Math.cos(radians);
+//         var y = radius * Math.sin(radians);
+
+//         // Path: may have to change to create a better triangle
+//         var mainPath = 'M -.0 -0.05 L .0 0.05 L ';
+//             pathX = String(x);
+//             space = ' ';
+//             pathY = String(y);
+//             pathEnd = ' Z';
+//         var path = mainPath.concat(pathX,space,pathY,pathEnd);
+
+//     var data = [{
+//           domain: { x: [0], y: [0] },
+//           title: { text: "Belly Button Washing Frequency <br> Scrubs per Week"},
+//           type: "scatter",
+//           marker: {size: 28, color:'850000'},
+//           showlegend: false,
+//           name: 'freq',
+//           text: level,
+//           hoverinfo: 'text+name'},
+//         { values: [50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50],
+//           rotation: 90, text: ["8-9", "7-8","6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0,1", ""], textinfo: 'text',
+//           textposition:'inside',      
+//           marker: {colors:['rgba(14, 127, 0, .5)', 'rgba(110, 154, 22, .5)',
+//                                  'rgba(170, 202, 42, .5)', 'rgba(202, 209, 95, .5)',
+//                                  'rgba(210, 206, 145, .5)', 
+//                                  'rgba(255, 255, 255, 0)']},
+//           labels: ['4.5-5', '3.5-4.49', '2.5-3.49', '1.5-2.49', '1-1.49'],
+//           hoverinfo: 'label',
+//           hole: .5,
+//           type: 'pie',
+//           showlegend: false
+//         }];
+      
+//       var layout = { width: 600, height: 450, margin: { t: 0, b: 0 } };
+//       Plotly.newPlot('gauge', data, layout);
+// });
+// }
 
 function demographics(id) {
     d3.json("samples.json").then(function(data) {
@@ -94,5 +141,3 @@ function demographics(id) {
 }
 
 init();
-
-
